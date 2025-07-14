c74dfc3b9c83c31ba47c9f7213c33caf1dfadfae
Timur <timgizzat@gmail>
2025-07-14 22:09:31 +0500

    fix

--- src/tests/cache.test.ts
+++ src/tests/cache.test.ts
@@ -1,9 +1,9 @@
 import { expect, describe, it } from "vitest";
 
-import { httpGet, httpPost } from "../http/request";
+import { httpGet } from "../http/request";
 import { setConfig } from "../core/config";
 import type { todo } from "./test-types";
-import { iShapeRQHooks } from "../types";
+import type { iShapeRQHooks } from "../types";
 
 const testConfig = () => {
   setConfig({
@@ -47,17 +47,5 @@ describe("ShapeRQ cache tests", () => {
     expect(
       await httpGet<todo>("Main", "/todos/1", { hooks: hook, cache: { ttl: 1000 } }),
     ).toStrictEqual(resp);
-  });
-  it("httpGet retry test", async () => {
-    let retryCount = 0;
-    const hook: iShapeRQHooks = {
-      onError: async ({ retry }) => {
-        if (retryCount < 2) {
-          retryCount++;
-          return await retry();
-        }
-      },
-    };
-    expect(await httpGet<todo>("Main", "/todos/0", { hooks: hook })).toStrictEqual(null);
   });
 });
